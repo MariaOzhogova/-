f73dd75c29df677a7b57292883da471240492a32
0CBET <Kuutamoloistaa@yandex.ru>
2026-03-10 12:38:35 +0800

    обновления

--- untitled/src/main/java/com/project/model/Model.java
+++ untitled/src/main/java/com/project/model/Model.java
@@ -1,29 +1,17 @@
 package com.project.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Model {
-    public double Intensity_0;
-    public double Intensity;
-    public double Alpha;
+    private double Intensity_0;
+    private double Intensity;
+    private double Alpha;
 
     public double calculate_Intensity() {
         Intensity = Intensity_0 * (Math.cos(Alpha)) * (Math.cos(Alpha));
         return Intensity;
     }
-
-    public void setAlpha(double alpha) {
-        Alpha = alpha;
-    }
-
-    public void setIntensity_0(double intensity_0) {
-        Intensity_0 = intensity_0;
-    }
-
-    public void setIntensity(double intensity) {
-        Intensity = intensity;
-    }
-
-    public double getIntensity() {
-        return Intensity;
-    }
-
 }
